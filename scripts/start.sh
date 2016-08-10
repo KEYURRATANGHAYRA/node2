6a38c5c5bdb722646025e237c3c5f3a52b89d933
hal2001so <hal2001so@outlook.com>
2016-08-10 13:36:49 -0400

    fourth commit thus far

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sudo service nginx start
-nohup node staticserver.js &
+service nginx start
+pid=$(ps -ef | grep node | grep staticserver.js | grep nohup | awk '{print $2}')
+if [ -z "$pid" ]
+then
+  screen -dmS nohup node /usr/share/nginx/html/staticserver.js &
+  fi
+
