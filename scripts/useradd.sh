b4b423e051af35e65451d42418f46f72d06b1e92
hal2001so <hal2001so@outlook.com>
2017-03-02 15:41:07 -0500

    updated useradd.sh

--- scripts/useradd.sh
+++ scripts/useradd.sh
@@ -5,5 +5,6 @@ getent passwd svp > /dev/null
 if [ $? -eq 0 ]; then
     echo "yes the user exists"
     else
-        echo "No, the user does not exist" ; useradd svp ; groupadd svp
-	fi
+        useradd svp; 
+	groupadd svp
+fi
