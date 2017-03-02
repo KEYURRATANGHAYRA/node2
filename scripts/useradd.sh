bff06170ed48284ac043942a724d80482852c13e
hal2001so <hal2001so@outlook.com>
2017-03-02 15:28:48 -0500

    changed useradd script to if statement

--- scripts/useradd.sh
+++ scripts/useradd.sh
@@ -1,2 +1,9 @@
-useradd svp
-groupadd svp
+#!/bin/bash
+
+getent passwd svp > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "yes the user exists"
+    else
+        echo "No, the user does not exist" ; useradd svp ; groupadd svp
+	fi
